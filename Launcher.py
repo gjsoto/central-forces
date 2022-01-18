2d3ede9cd64732b5b0c45b9e93c0b83dfe858bba
Gab <gjsoto3@gmail.com>
2022-01-18 01:06:37 -0500

    more comments

--- Launcher.py
+++ Launcher.py
@@ -48,6 +48,7 @@ class Launcher(object):
     
     def createParameterButton(self):
         
+        # call function when switching radio buttons for parameters
         def param( label ):
             if label == 'e = 0':
                 self.e = 0
@@ -70,9 +71,14 @@ class Launcher(object):
             self.plotRadialOrbit( label )
             self.plotPotentialEnergy( label )
         
+        # create axis object for parameter button
         ax_param = self.fig.add_axes([0.02, 0.4, 0.1, 0.25])
+        
+        # create parameter button with labels
         self.param_button = RadioButtons(ax_param, ['e = 0', 'e < 1', 'e = 1', 'e > 1'],
                             [True, False, False, False], activecolor= 'r')
+        
+        # keep parameter button at the ready and define call function
         self.param_button.on_clicked(param)
         plt.show()
     
@@ -88,78 +94,106 @@ class Launcher(object):
             valmax=1,
             valinit=0,
         )
+        
+        # turning on the slider and keeping it ready for updates
         self.angle_slider.on_changed(self.updateSlider)
         plt.show()
 
     
     def plotRadialOrbit(self, label):
         
+        # get orbit and angles for orbit + color for orbit
         r   = self.orbiter.orbit
         phi = self.orbiter.phi
+        color = self.color
+        
+        # normalize angles for interpolation
         phi_norm = phi - phi.min() 
         phi_norm /= phi_norm.max()
+        
+        # create radial interpolant
         self.r_interp = interp1d( phi_norm, r, kind='cubic')
         
+        # calculate x- and y-components for orbit + interpolants
         x_ = r*np.cos(phi)
         y_ = r*np.sin(phi)
         self.x_interp = interp1d( phi_norm, x_, kind='cubic')
         self.y_interp = interp1d( phi_norm, y_, kind='cubic')
         
-        color = self.color
-        
+        # plot orbit
         self.axR.plot( x_, y_, color=color )
+        
+        # plot origin of coordinates
         self.axR.plot( [0], [0], 'kx', markersize=5  )
         self.axR.plot( [0], [0], 'wx', markersize=2  )
+        
+        # create orbiter point
         self.point, = self.axR.plot( [self.x_interp(0)], [self.y_interp(0)], 'ko', markersize=5  )
         
+        # set axis labels and limits
         self.axR.set_xlabel('X-Component')
         self.axR.set_ylabel('Y-Component')
-        
         self.axR.set_xlim( -3 , 3 )
         self.axR.set_ylim( -3 , 3 )
 
 
     def plotPotentialEnergy(self, label):
         
+        # get orbit, angles, and energy for orbit + color for orbit
         r   = self.orbiter.orbit
         phi = self.orbiter.phi
         Ubar = self.orbiter.energy
-        
-        phi_norm = phi - phi.min()
-        phi_norm /= phi_norm.max()
-        Ubar_max  = self.orbiter.energy_max
-        Ubar_prof = self.orbiter.energy_profile
-        r_prof    = self.orbiter.r_profile
-        zero_line = np.zeros( len(r_prof) )
-        
-        self.u_interp = interp1d( phi_norm, Ubar, kind='cubic')
-        
         color = self.color 
         
+        # normalize angles for interpolation
+        phi_norm = phi - phi.min()
+        phi_norm /= phi_norm.max()
+        
+        # get max and profile energy curves for plotting
+        r_prof    = self.orbiter.r_profile
+        Ubar_max  = self.orbiter.energy_max
+        Ubar_prof = self.orbiter.energy_profile
+        
+        # create zero energy line demarcating bound vs unbound orbits
+        zero_line = np.zeros( len(r_prof) )
+        
+        # calculate energy interpolant
+        self.u_interp = interp1d( phi_norm, Ubar, kind='cubic')
+        
+        # plotting general energy profiles
         self.axU.plot( r_prof, zero_line, '--', color='k' )
         self.axU.plot( r_prof, Ubar_max, '--', color=color )
         self.axU.plot( r_prof, Ubar_prof, color='k' )
-        self.axU.plot( r, Ubar, color=color )
         
+        # text demarcating bound and unbound orbits
         self.axU.text( 2.5,  0.025, "UNBOUND ORBIT")
         self.axU.text( 2.5, -0.05,  "BOUND ORBIT")
         
+        # plot actual orbit energy
+        self.axU.plot( r, Ubar, color=color )
+        
+        # set axis labels
         self.axU.set_xlabel('Orbital Radius')
         self.axU.set_ylabel('Effective Potential Energy')
         
+        # create orbiter point for energy plot
         self.Upoint, = self.axU.plot( [self.r_interp(0)], [self.u_interp(0)], 'o', color=color, markersize=5  )
 
 
     def resetPlots(self):
         
+        # clearing axes
         self.axR.cla()
         self.axU.cla()
         
         
     def updateSlider(self, phival):
+        
+        # update location of orbiter
         self.point.set_xdata(self.x_interp(phival))
         self.point.set_ydata(self.y_interp(phival))
-
+        
+        # update location of potential energy point
         self.Upoint.set_xdata(self.r_interp(phival))
         self.Upoint.set_ydata(self.u_interp(phival))
         
